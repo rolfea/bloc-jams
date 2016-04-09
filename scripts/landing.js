971064439013534c4f3b28842d5b34011323e9be
Alex Rolfe <rolfepercussion@gmail.com>
2016-04-09 13:02:43 -0500

    Add event handlers for scroll

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,13 +1,29 @@
+var pointsArray = document.getElementsByClassName('point');
+
 var animatePoints = function() {
-  var points = document.getElementsByClassName('point');
+  var revealPoint = function(index) {
+    pointsArray[index].style.opacity = 1;
+    pointsArray[index].style.transform = "scaleX(1) translateY(0)";
+    pointsArray[index].style.msTransform = "scaleX(1) translateY(0)";
+    pointsArray[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+  }
   
-  var revealPoints = function() {
-    for (var i = 0; i < points.length; i++) {
-      points[i].style.opacity = 1;
-      points[i].style.transform = "scaleX(1) translateY(0)";
-      points[i].style.msTransform = "scaleX(1) translateY(0)";
-      points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+  for (var i = 0; i < pointsArray.length; i++) {
+    revealPoint(i);
+  }
+};
+
+window.onload = function() {
+  if (window.innerHeight > 950) {
+    animatePoints(pointsArray);
+  }
+  
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+  
+  window.addEventListener('scroll', function(event) {
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+      animatePoints(pointsArray);
     }
-  };
-  revealPoints();
-};    
+  });
+}
